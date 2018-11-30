80ee207c02739e07bf14f04fdd44c25a11d53823
sneuly <a_sanakoevi@cu.edu.ge>
2018-11-30 02:41:37 +0100

    thread safety

--- ThreadilyTransmittedDiseases/Program.cs
+++ ThreadilyTransmittedDiseases/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ThreadilyTransmittedDiseases
 {
     class Program
     {
+        private static readonly Dictionary<int, string> Items = new Dictionary<int, string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var task = Task.Factory.StartNew(AddItem);
+            var task2 = Task.Factory.StartNew(AddItem);
+            var task3 = Task.Factory.StartNew(AddItem);
+            var task4 = Task.Factory.StartNew(AddItem);
+            var task5 = Task.Factory.StartNew(AddItem);
+            Task.WaitAll(task, task2, task3, task5, task4);
+        }
+
+        private static void AddItem()
+        {
+            lock (Items)
+            {
+                Console.WriteLine("Lock acquired by " + Task.CurrentId);
+                Items.Add(Items.Count, "BS " + Items.Count);
+            }
+
+            lock (Items)
+            {
+                Console.WriteLine("Lock 2 acquired by " + Task.CurrentId);
+                var readOnlyItems = Items;
+                foreach (var kvp in readOnlyItems)
+                {
+                    Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+            }
+
         }
     }
 }
