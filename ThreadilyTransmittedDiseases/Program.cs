ebc132390a128570f85c406a5147c0458c73beea
sneuly <a_sanakoevi@cu.edu.ge>
2018-11-30 02:07:21 +0100

    Add project files.

---
+++ ThreadilyTransmittedDiseases/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadilyTransmittedDiseases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
